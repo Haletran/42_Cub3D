f0d43edc1b88be6e1d721fbab0ae89095a3de010
Haletran <bpasquier123@gmail.com>
2024-08-11 22:00:10 +0200

    fix: minimap render working again

--- src/minimap.c
+++ src/minimap.c
@@ -6,29 +6,25 @@
 /*   By: bapasqui <bapasqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 13:26:40 by qdeviann          #+#    #+#             */
-/*   Updated: 2024/08/11 21:50:00 by bapasqui         ###   ########.fr       */
+/*   Updated: 2024/08/11 21:59:52 by bapasqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	draw_square2(t_mlx *mlx, int j, int i, int color)
+void	draw_square2(t_mlx *mlx, t_xy *xy, t_xy *ij, int color)
 {
-	int	x;
-	int	y;
-
-	x = j * 10;
-	y = i * 10;
-	while (x < (((j + 1) * 10)))
+	while (xy->x > (ij->y * 10))
 	{
-		mlx_set_image_pixel(mlx->mlx, mlx->minimap->img, x + 5, (i * 10) + 5,
+		mlx_set_image_pixel(mlx->mlx, mlx->minimap->img, xy->x + 5, xy->y,
 			color);
-		x++;
+		xy->x--;
 	}
-	while (y < (((i + 1) * 10)))
+	while (xy->y > (ij->x * 10))
 	{
-		mlx_set_image_pixel(mlx->mlx, mlx->minimap->img, x + 5, y + 5, color);
-		y++;
+		mlx_set_image_pixel(mlx->mlx, mlx->minimap->img, (ij->y * 10) + 5,
+			xy->y, color);
+		xy->y--;
 	}
 }
 
@@ -50,7 +46,7 @@ void	draw_square(t_mlx *mlx, int j, int i, int color)
 		mlx_set_image_pixel(mlx->mlx, mlx->minimap->img, x + 5, y, color);
 		y++;
 	}
-	draw_square2(mlx, j, i, color);
+	draw_square2(mlx, &(t_xy){x, y}, &(t_xy){i, j}, color);
 }
 
 void	draw_minimap(t_mlx *mlx)
